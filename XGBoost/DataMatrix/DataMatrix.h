5382a6643d3457913c78eeea0c7a989612a80163
Charlie Koenig <charliek0303@gmail.com>
2025-03-02 15:29:51 -0500

    Implemented intial DM Operations

--- XGBoost/DataMatrix/DataMatrix.h
+++ XGBoost/DataMatrix/DataMatrix.h
@@ -19,24 +19,37 @@ public:
 
     // Constructors
     DataMatrix() = default;
-
     DataMatrix(const vector<string>& features, vector<vector<double>>& data);
     DataMatrix(const vector<string>& features);
+    
+    // CSV to DataMatrix
+    static DataMatrix from_csv(string filepath, const char deliminator = ',');
+    static DataMatrix from_csv(string filepath, unsigned int num_columns, const char deliminator = ',');
+    static DataMatrix from_csv(string filepath, const vector<string>& column_names, const char deliminator = ',');
 
     void add_row(vector<double>& row);
+    void insert_row(vector<double>& row, int at);
 
-    void add_column(const string& feature_name, vector<double>&& column);
-    // void add_column(vector<double>&& col);
+    void add_column(const string& label, vector<double>&& column);
+    void add_columns(vector<string>& labels, vector<vector<double>>&& columns);
+    void add_column(vector<double>&& column);
+    void add_columns(vector<vector<double>>&& columns);
 
-    // void remove_column(const string& label);
-    // void remove_column(int index);
 
-    // vector<double>& operator[](const string& label);
-    // vector<double>& operator[](int index);
+    void insert_column(const string& label, vector<double>&& column, int at);
+    void insert_column(vector<double>&& column, int at);
+
+    void remove_column(const string& label);
+    void remove_column(int index);
+
+    vector<double>& operator[](const string& label);
+    vector<double>& operator[](int index);
 
     int num_rows() const;
     int num_cols() const;
-    // vector<string> get_features() const;
+    vector<string> columns() const;
+
+    string head(int num_rows = 10) const;
 };
 
 
